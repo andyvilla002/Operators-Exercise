23e985b5397ecde9940d9c887133f67fb73d0b97
Andy Villalobos <Andy.villa012@gmail.com>
2023-02-01 21:14:41 -0800

    initial operatos commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,48 @@
 {
     public class Program
     {
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise1
+
+            //Addition Operator
+            int x = 10;
+            int y = 20;
+            x += y;
+            Console.WriteLine(x);
+
+            //Subtraction Operator
+            x = 10;
+            y = 5;
+            x -= y;
+            Console.WriteLine(x);
+
+            //Multiplication Operator
+            x = 10;
+            y = 5;
+            x *= y;
+            Console.WriteLine(x);
+
+            //Division Operator
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine($"What is the radius of your circle ?");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
         }
-    }
+    }   
 }
